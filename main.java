806c462a50a7914a3c989380c56667424aea405a
Jack Jin <jackjin@live.com>
2024-08-16 04:14:19 +0000

    testing out conflicts

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class MainClass{
   public static void main(String[] args){
     System.out.println("Today is a good day!");
-    System.out.println("Test out branch_1");
+    System.out.println("Test out conflict");
   }
 }
